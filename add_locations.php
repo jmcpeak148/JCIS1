00e3a7fed65c3a3e63858747495dd1a249262de9
jmcpeak148 <79474838+jmcpeak148@users.noreply.github.com>
2021-02-22 16:12:42 -0500

    Add files via upload

---
+++ add_locations.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta name="dashboard" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1"> <!-- Tells the browser to be responsive to screen width -->
+	
+	<title>Joes's Caribe Locations</title>	
+	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <!-- Menu Icons -->
+</head>
+
+<body>
+	<?php
+		readfile("../menu/sidebar.inc");
+	?>
+	
+<div id="right" class="column">
+
+    <div class="top-right">
+		<a href="#user"><i class="fa fa-user-circle fa-fw"></i>Inventory</h3></a>
+	</div>
+	
+	<div class="bottom">
+		<div class="main">
+		
+			<p> List of inventory here </p>
+				
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
